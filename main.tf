dc518f2a90c80bb36f9f331373be8a946bc35274
EC2 Default User <ec2-user@ip-172-31-42-3.eu-west-1.compute.internal>
2021-07-21 08:13:58 +0000

    revert instance size

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "vm_tf" {
   provider      = aws.prod
   ami           = data.aws_ssm_parameter.prod_ami.value
   subnet_id     = aws_subnet.subnet_1.id
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
   tags = {
     Name = "vm-tf"
   }
